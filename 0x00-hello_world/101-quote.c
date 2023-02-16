e09899f842b603a042076f7e5fc5519421c13257
Olajide <larrm_ola@live.com>
2023-02-16 13:14:01 -0800

    Quoting using C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,8 @@
-#include <stdio.h>
+#include <unistd.h>
 
-/**
- * main - Print Message
- *
- * Return: Always 1 (Success)
+/**main - prints "and that piece of art is useful" - Dora Kprpar, 2015-10=19",
+ * followed by a newline, to standard error.
+ * Return: Always 1. (Success)
  */
 int main(void)
 {
