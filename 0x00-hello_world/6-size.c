57a786187e9043b0fe38b8833b1f70815fc15489
Olajide <larrm_ola@live.com>
2023-02-16 10:27:38 -0800

    Printing sizes of various types of computers

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-	printf("size of char is=%i byte(s)\n", sizeof(char));
-	printf("size of short is=%i byte(s)\n", sizeof(short));
-	printf("size of int is=%i byte(s)\n", sizeof(int));
-	printf("size of long is=%i byte(s)\n", sizeof(long));
+	printf("size of char is= %zu byte(s)\n", sizeof(char));
+	printf("size of short is= %zu byte(s)\n", sizeof(short));
+	printf("size of int is= %zu byte(s)\n", sizeof(int));
+	printf("size of long is= %zu byte(s)\n", sizeof(long));
 
 	return (0);
 }
