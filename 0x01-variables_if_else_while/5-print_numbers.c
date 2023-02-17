37afee855c6021204bc6e5c8fe5a9a564e3534e4
Olajide <larrm_ola@live.com>
2023-02-17 11:53:29 -0800

    Printing single digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Programme that prints single digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int 10 = 0;
+
+	while (10 > 0; 10++)
+	printf(10'\n')
+
+	return (0)
+}
