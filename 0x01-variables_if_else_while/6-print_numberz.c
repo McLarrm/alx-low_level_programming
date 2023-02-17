e7b25ac0c83179c7f7ea85832a1314f0b7c005a7
Olajide <larrm_ola@live.com>
2023-02-17 12:17:31 -0800

    Printing single digit numbers without printf

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Printing numbers without char
+ *
+ * Return Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	if (n = 0; n < 10; n++)
+	putchar(n);
+	putchar('\n');
+
+	return (0);
+}
