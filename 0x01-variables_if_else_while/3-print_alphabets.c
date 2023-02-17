357ea183eb9dfa299b96055716ed30b8a91175de
Olajide <larrm_ola@live.com>
2023-02-17 08:29:43 -0800

    Printing in lower and uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio>
+/**
+ * main - Printing alphabet in lower and uppercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+	
+	if (ch = 'a'; ch <= 'z'; ch++)
+	putchar(ch);
+	else (ch = 'A'; ch <= 'Z'; ch++)
+	putchar(ch);
+	for (ch = 'a''A'; ch <= 'z''Z'; ch++)
+	putchar('\n);
+
+	return (0);
+}
