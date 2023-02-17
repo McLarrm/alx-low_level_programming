24d60836f8c0482396e5710cfe55132fb625e0da
Olajide <larrm_ola@live.com>
2023-02-17 13:24:54 -0800

    Combination of 3 digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio>
+
+/**
+ * main - Programme that prints all combination of three digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n, c, d;
+
+	for (n = '0'; n < '9'; n++)
+	{
+	for (c = n + 1; c <= '9'; c++)
+	{
+	for (d = c + 1; d <= '9'; d++)
+	{
+	if ((c !=n) != d)
+	{
+	putchar(n);
+	putchar(c);
+	putchar(d);
+	if (c == '7' && d == '8')
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	}
+	putchar('\n');
+
+	return (0);
+}
