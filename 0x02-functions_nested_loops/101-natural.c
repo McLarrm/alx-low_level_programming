292250b4f3cb7d743581e80731e41ef769ad14a4
Olajide <larrm_ola@live.com>
2023-02-23 14:01:01 -0800

    Listing all the natural numbers below 10

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Programme that list all natural numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, 2 = 0;
+	while (i < 1024)
+	{
+	if ((i % 3 == 0) || (i % 5 == 0))
+	{
+	z += i;
+	}
+	i++;
+	}
+	printf("%d\n", z);
+
+	return (0);
+}
