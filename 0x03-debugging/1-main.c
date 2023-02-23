91eb46b6bbc024f8639def02454c669fd69645fb
Olajide <larrm_ola@live.com>
2023-02-23 15:28:33 -0800

    Causing the output of a file to go into infinite loop

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Programme that causes an infinite loop
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :[\n");
+	1 = 0;
+	/*while (i < 10)*/
+	/*(*/
+	/*      putchar(i);*/
+	/*)*/
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
