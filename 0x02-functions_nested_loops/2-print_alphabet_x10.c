773c61300c375baee76d0f1048ab5adbdd0cd94a
Olajide <larrm_ola@live.com>
2023-02-23 09:52:41 -0800

    Printing the alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Makig the alphabet 10x 
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char a;
+	int i = 0;
+
+	while (i <= 9)
+	{
+	for (a = 'a'; a <= 'z'; a++)
+	}
+	_putchar(a);
+
+	putchar('\n');
+	i++;
+}
