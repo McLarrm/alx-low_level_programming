f49fdf769966ee0e767bf515ce5f56096b889506
Olajide <larrm_ola@live.com>
2023-02-24 04:25:02 -0800

    Function that prints 0 to 9

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,7 +3,7 @@
 /**
  * mul - Multiplies two integers
  * @a: The first number to multiply
- * @b: The second number to multiply 
+ * @b: The second number to multiply
  * Return: The result of the multiplication
  */
 int mul(int a, int b)
