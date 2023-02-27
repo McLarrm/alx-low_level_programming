1cc9689f7f0b71c27017c99692e6bf67d58f8d2c
Olajide <larrm_ola@live.com>
2023-02-27 09:57:31 -0800

    Prime factor of numbers

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * maint - Printing the largest prime factor of a number
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long p, maxf;
+	long number = 612852475143;
+	double square = sqrt(number);
+
+	for (p = 1; p <= square; p++)
+	{
+	if (number % p == 0)
+	{
+	maxf = number / x;
+	}
+	}
+	printf("%ld\n", maxf);
+
+	return (0);
+}
