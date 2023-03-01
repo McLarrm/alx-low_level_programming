553947a256783cfab4c069aee729b593ab893e7d
Olajide <larrm_ola@live.com>
2023-03-01 09:25:43 -0800

    Concatenates two strings

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,8 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
-
 char *_strcat(char *dest, char *src);
 char *strncat(char *dest, char *src, int n);
 char *strncpy(char *dest, char *src, int n);
@@ -15,4 +10,3 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
-#endif
