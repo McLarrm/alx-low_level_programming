b2ad03da74af08d7cd70760a396a3b1ab1193fac
Olajide <larrm_ola@live.com>
2023-03-06 12:15:29 -0800

    Function that gets the length of a prefix

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,11 +1,10 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * _strspn - Gets the length of a prefix
+ * _strspn - Function that gets the length of prefix
  * @s: Input
- * @accept: Input
- * Return: Always 0
+ * @accept: Substring
+ * Return: Always 0 (Success)
  */
 unsigned int _strspn(char *s, char *accept)
 {
@@ -16,7 +15,60 @@ unsigned int _strspn(char *s, char *accept)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 	check = 0;
+	for (n = 0; accept[n] != '\0'; n++)
+	{
+	if (accept[n] == s[i])
+	{
+	value++;
+	check = 1;
+	}
+	}
+	if (check == 0)
+	}
+}#include "main.h"
 
+/**
+ * _strspn - Function that gets the length of prefix
+ * @s: Input
+ * @accept: Substring
+ * Return: Always 0 (Success)
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, n, value, check;
+
+	value = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	check = 0;
+	for (n = 0; accept[n] != '\0'; n++)
+	{
+	if (accept[n] == s[i])
+	{
+	value++;
+	check = 1;
+	}
+	}
+	if (check == 0)
+	}
+}#include "main.h"
+
+/**
+ * _strspn - Function that gets the length of prefix
+ * @s: Input
+ * @accept: Substring
+ * Return: Always 0 (Success)
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, n, value, check;
+
+	value = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	check = 0;
 	for (n = 0; accept[n] != '\0'; n++)
 	{
 	if (accept[n] == s[i])
