4bd86931935c4a7156f3b0d5934154c4c9f9ca9b
Olajide <larrm_ola@live.com>
2023-03-06 13:01:44 -0800

    Function that gets the length of a prefix

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,60 +8,6 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, n, value, check;
-
-	value = 0;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-	check = 0;
-	for (n = 0; accept[n] != '\0'; n++)
-	{
-	if (accept[n] == s[i])
-	{
-	value++;
-	check = 1;
-	}
-	}
-	if (check == 0)
-	}
-}#include "main.h"
-
-/**
- * _strspn - Function that gets the length of prefix
- * @s: Input
- * @accept: Substring
- * Return: Always 0 (Success)
- */
-unsigned int _strspn(char *s, char *accept)
-{
-	unsigned int i, n, value, check;
-
-	value = 0;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-	check = 0;
-	for (n = 0; accept[n] != '\0'; n++)
-	{
-	if (accept[n] == s[i])
-	{
-	value++;
-	check = 1;
-	}
-	}
-	if (check == 0)
-	}
-}#include "main.h"
-
-/**
- * _strspn - Function that gets the length of prefix
- * @s: Input
- * @accept: Substring
- * Return: Always 0 (Success)
- */
-unsigned int _strspn(char *s, char *accept)
-{
 	unsigned int n = 0;
 	int r;
 
