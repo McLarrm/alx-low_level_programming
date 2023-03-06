8c3fb16c0953d209eb3e43c4deffce23516c0a95
Olajide <larrm_ola@live.com>
2023-03-06 14:12:58 -0800

    Function that prints the sum of two diagonal integers

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -18,7 +18,7 @@ void print_diagsums(int *a, int size)
 		sum += *ptr;
 		ptr += size + 1;
 	}
-	printf("%d,", sum);
+	printf("%d, ", sum);
 	sum = 0;
 	ptr = a + size - 1;
 	for (n = size; n > 0; n--)
