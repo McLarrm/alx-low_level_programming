03522c6a5b8b130d55783bddfd2093fb1e621e0c
Olajide <larrm_ola@live.com>
2023-03-08 16:40:56 -0800

    Function that returns a prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,15 +9,13 @@
  */
 int prime_finder(int n, int x)
 {
-	int p;
-
 	if (n == x)
 	{
 		return (1);
 		if (!(n % x))
 			return (0);
 	}
-	p = prime_finder(n, x + 1);
+	return (prime_finder(n, x + 1));
 }
 /**
  * is_prime_number - Returning the input of a prime number
@@ -28,5 +26,5 @@ int is_prime_number(int n)
 {
 	if (n < 2)
 		return (0);
-	return (prime_fider(n, 2));
+	return (prime_finder(n, 2));
 }
