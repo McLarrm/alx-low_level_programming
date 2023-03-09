c652add224a11b41b10723dee6fa67ce9e7f6504
Olajide <larrm_ola@live.com>
2023-03-08 17:37:27 -0800

    Function that compares two strings

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * exactmode - Comparing two strings
+ * @s1: First string
+ * @s2: Second string
+ * Return: 1
+ */
+int exactmode(char *s1, char *s2)
+{
+	if (*s2 == '*')
+		return (wildmode(s1, s2 + 1));
+	if (*s1 == 0 && *s2 == 0)
+		return (1);
+	if (*s1 == *s2)
+		return (exactmode(s1 + 1, s2 + 1));
+	return (0);
+}
+/**
+ * wildmore - Comparing two strings
+ * @s1: First string
+ * @s2: Second string
+ * Return: 1
+ */
+int wildmode(char *s1, char *s2)
+{
+	if (*s2 == '*')
+		return (wildmode(s1, s2 + 1));
+	if (*s2 == 0)
+		return (1);
+	if (*s1 == 0)
+		return (0);
+	if (*s1 != *s2)
+		return (wildmode(s1 + 1, s2));
+	if (!(exactmode(s1 + 1, s2 + 1)))
+		return (wildmode(s1 + 1, s2));
+	return (1);
+}
+/**
+ * wildcmp - Comparing two strings
+ * @s1: First string
+ * @s2: Second string
+ * Return: 1
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s2 == '*')
+		return (wildmode(s1, s2));
+	return (exactmode(s1, s2));
+}
