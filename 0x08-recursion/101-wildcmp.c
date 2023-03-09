622c88342f206ed914f52f207a419cbf58ab5dc9
Olajide <larrm_ola@live.com>
2023-03-08 17:45:33 -0800

    Function that compares two strings

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -24,7 +24,7 @@ int exactmode(char *s1, char *s2)
 	return (0);
 }
 /**
- * wildmore - Comparing two strings
+ * wildmode - Comparing two strings
  * @s1: First string
  * @s2: Second string
  * Return: 1
