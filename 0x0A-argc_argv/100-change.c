1fc280786090fce8744c71b8d9998da08052ee07
Olajide <larrm_ola@live.com>
2023-03-10 10:04:34 -0800

    Programme that prints the minimum number of a coins

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,36 +10,36 @@
  */
 int main(int argc, char *argv[])
 {
-	int cent, coin = 0;
+	int cents, coins = 0;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	cent = atoi(argv[1]);
-	while (cent >= 25)
+	cents = atoi(argv[1]);
+	while (cents >= 25)
 	{
-		coin++;
-		cent -= 25;
+		coins++;
+		cents -= 25;
 	}
-	while (cent >= 10)
+	while (cents >= 10)
 	{
-		coin++;
-		cent -= 10;
+		coins++;
+		cents -= 10;
 	}
-	if (cent >= 5)
+	if (cents >= 5)
 	{
-		coin++;
-		cent -=5;
+		coins++;
+		cents -=5;
 	}
-	while (cent >= 2)
+	while (cents >= 2)
 	{
-		coin++;
-		cent -= 2;
+		coins++;
+		cents -= 2;
 	}
-	if (cent == 1)
-		coin++;
+	if (cents == 1)
+		coins++;
 	printf("%d\n", coins);
 	return (0);
 }
