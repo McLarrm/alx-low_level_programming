c75238c17099dbc70decbd26d71613988a3abb32
Olajide <larrm_ola@live.com>
2023-03-13 16:46:43 -0700

    Function that returns a pointer to 2 dimensional integers

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,7 +12,7 @@ int **alloc_grid(int width, int height)
 {
 	int **dmsn, i, s;
 
-	if (width <= 0 || height <+ 0)
+	if (width <= 0 || height <= 0)
 		return (NULL);
 	dmsn = malloc(height * sizeof(int *));
 	if (dmsn == NULL)
