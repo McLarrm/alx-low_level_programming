3c8ee25125958dd2ed742cfc1f575f8b742e1b6c
Olajide <larrm_ola@live.com>
2023-03-16 13:58:09 -0700

    Functin that concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -30,7 +30,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	pointer = ret;
 	while (*s1)
 		*pointer++ = *s1++;
-	int i = 0;
+	unsigned int i = 0;
+
 	while (i < n)
 	{
 		*pointer++ = s2[i++];
