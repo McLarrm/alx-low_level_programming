947bdc9f58694d435d965543ad1a547f9c9ff4c7
Olajide <larrm_ola@live.com>
2023-03-22 14:21:51 -0700

    Program that prints the opcodes of its own function

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print hex opcodes
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	unsigned char *f = (unsigned char *) (long int) main;
+	int n, i;
+
+	if (ac != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	n = atoi(av[1]);
+	if (n < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+	if (n > 0)
+		printf("%02x", *f++);
+	for (i = 1; i < n; i++)
+		printf(" %02x", *f++);
+	if (n > 0)
+		printf("\n");
+	return (0);
+}
