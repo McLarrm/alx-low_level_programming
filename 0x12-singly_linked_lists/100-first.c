8c835b0e9b56b6f59e8951be82300dcb0bea23ae
Olajide <larrm_ola@live.com>
2023-03-30 18:43:52 -0700

    Function that prints a message before the main function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * turtle - Prints before main
+ * Return: Noting
+ */
+void __attribute__ ((constructor)) turtle()
+{
+	printf("You're beat! and yet, you must allow,\n""I bore my house upon my back!\n");
+}
