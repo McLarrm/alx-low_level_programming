692c117e99087b16429e7d0760bd94bce016462b
Olajide <larrm_ola@live.com>
2023-03-30 18:58:07 -0700

    My first assembly code

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,20 +1,19 @@
-section .data
-    format db "Hello, Holberton\n",0
+    section .data
+hello:	db "Hello, Holberton",10,0
+format:	db "%s",0
 
-section .text
+    section .text
     global main
-
     extern printf
 
 main:
     push rbp
     mov rbp, rsp
 
-    lea rdi, [format]
-    xor eax, eax
+    mov rsi, hello
+    mov rdi, format
     call printf
 
-    mov rsp, rbp
     pop rbp
-    xor eax, eax
+    mov rax, 0
     ret
