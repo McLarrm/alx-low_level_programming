18a985374a22283ecdece03ff8541e9253fe3cb9
Olajide <larrm_ola@live.com>
2023-04-04 03:46:58 -0700

    Function that prints a linked list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,47 +1,46 @@
+#include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "lists.h"
 
 /**
- * is_node_in_list - Checks if a node is in a linked list
- * @head: Pointer to the head of the linked list
- * @node: Pointer to the node
- * Return: 1 if node is in the list, 0 otherwise
- */
-int is_node_in_list(const listint_t *head, const listint_t *node)
-{
-	const listint_t *current = head;
-	while (current != NULL)
-	{
-		if (current == node)
-			return (1);
-		current = current->next;
-	}
-	return (0);
-}
-/**
- * print_listint_safe - Prints a linked list of integers
- * @head: Pointer to the head of the linked list
- * Return: The number of nodes in the list
+ * print_listint_safe - Prints a listint_t linked list safely
+ * @head: Pointer to the beginning of the list
+ * Return: Number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t count = 0;
+    const listint_t *slow, *fast;
+    size_t count = 0;
 
-	while (head != NULL)
-	{
-		printf("[%p] %d\n", (void *) head, head->n);
-		count++;
-		if (head <= head->next)
-		{
-			printf("-> [%p] %d\n", (void *) head->next, head->next->n);
-			break;
-		}
-		head = head->next;
-	}
+    if (head == NULL)
+        exit(98);
 
-	if (count == 0)
-		exit(98);
+    slow = head;
+    fast = head;
 
-	return (count);
+    while (fast != NULL && fast->next != NULL)
+    {
+        printf("[%p] %d\n", (void *)slow, slow->n);
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast)
+        {
+            slow = head;
+            while (slow != fast)
+            {
+                printf("[%p] %d\n", (void *)slow, slow->n);
+                slow = slow->next;
+                fast = fast->next;
+            }
+            printf("[%p] %d\n", (void *)slow, slow->n);
+            exit(98);
+        }
+        count++;
+    }
+
+    printf("[%p] %d\n", (void *)slow, slow->n);
+    count++;
+
+    return (count);
 }
