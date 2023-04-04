69f5bcc9b700971d5aa716158ce7db5fd8b11b81
Olajide <larrm_ola@live.com>
2023-04-04 03:43:27 -0700

    Function that prints a linked list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,6 +3,23 @@
 #include "lists.h"
 
 /**
+ * is_node_in_list - Checks if a node is in a linked list
+ * @head: Pointer to the head of the linked list
+ * @node: Pointer to the node
+ * Return: 1 if node is in the list, 0 otherwise
+ */
+int is_node_in_list(const listint_t *head, const listint_t *node)
+{
+	const listint_t *current = head;
+	while (current != NULL)
+	{
+		if (current == node)
+			return (1);
+		current = current->next;
+	}
+	return (0);
+}
+/**
  * print_listint_safe - Prints a linked list of integers
  * @head: Pointer to the head of the linked list
  * Return: The number of nodes in the list
