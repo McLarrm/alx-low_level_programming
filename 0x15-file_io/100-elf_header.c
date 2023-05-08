6f2a81973e9a32622f7fc81ab44ae2e9059e7110
Olajide <larrm_ola@live.com>
2023-05-08 07:55:07 -0700

    Program that displays the info contained in the ELF header at the start of an ELF file

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -10,8 +10,8 @@
 #define ERR_SYSCALL(syscall) ERR("%s failed: %s", #syscall, strerror(errno))
 
 /**
- * print_elf_header - Prints an error message and exits with error code 98
- * @header: Error message to be printed
+ * print_elf_header - Prints the ELF header information
+ * @header: pointer to the ELF header
  */
 void print_elf_header(Elf64_Ehdr *header)
 {
@@ -19,61 +19,142 @@ void print_elf_header(Elf64_Ehdr *header)
 
 	printf("ELF Header:\n");
 	printf("  Magic:   ");
-
 	for (i = 0; i < EI_NIDENT; i++)
 	{
 		printf("%02x ", header->e_ident[i]);
 	}
-
 	printf("\n");
 	printf("  Class:                             ");
-
 	switch (header->e_ident[EI_CLASS])
 	{
-		case ELFCLASSNONE: printf("Invalid class\n"); break;
-		case ELFCLASS32:   printf("ELF32\n"); break;
-		case ELFCLASS64:   printf("ELF64\n"); break;
-		default:           printf("Unknown\n"); break;
+		case ELFCLASSNONE:
+			printf("Invalid class\n");
+			break;
+		case ELFCLASS32:
+			printf("ELF32\n");
+			break;
+		case ELFCLASS64:
+			printf("ELF64\n");
+			break;
+		default:
+			printf("Unknown\n");
+			break;
 	}
 	printf("  Data:                              ");
-
 	switch (header->e_ident[EI_DATA])
 	{
-		case ELFDATANONE: printf("Invalid data encoding\n"); break;
-		case ELFDATA2LSB: printf("2's complement, little endian\n"); break;
-		case ELFDATA2MSB: printf("2's complement, big endian\n"); break;
-		default:          printf("Unknown\n"); break;
+		case ELFDATANONE:
+			printf("Invalid data encoding\n");
+			break;
+		case ELFDATA2LSB:
+			printf("2's complement, little endian\n");
+			break;
+		case ELFDATA2MSB:
+			printf("2's complement, big endian\n");
+			break;
+		default:
+			printf("Unknown\n");
+			break;
 	}
 	printf("  Version:                           %d (current)\n", header->e_ident[EI_VERSION]);
 	printf("  OS/ABI:                            ");
-
 	switch (header->e_ident[EI_OSABI])
 	{
-		case ELFOSABI_SYSV:        printf("UNIX - System V\n"); break;
-		case ELFOSABI_HPUX:        printf("UNIX - HP-UX\n"); break;
-		case ELFOSABI_NETBSD:      printf("UNIX - NetBSD\n"); break;
-		case ELFOSABI_LINUX:       printf("UNIX - Linux\n"); break;
-		case ELFOSABI_SOLARIS:     printf("UNIX - Solaris\n"); break;
-		case ELFOSABI_IRIX:        printf("UNIX - IRIX\n"); break;
-		case ELFOSABI_FREEBSD:     printf("UNIX - FreeBSD\n"); break;
-		case ELFOSABI_TRU64:       printf("UNIX - TRU64\n"); break;
-		case ELFOSABI_ARM:         printf("ARM\n"); break;
-		case ELFOSABI_STANDALONE:  printf("Standalone App\n"); break;
-		default:                   printf("Unknown\n"); break;
+		case ELFOSABI_SYSV:
+			printf("UNIX - System V\n");
+			break;
+		case ELFOSABI_HPUX:
+			printf("UNIX - HP-UX\n");
+			break;
+		case ELFOSABI_NETBSD:
+			printf("UNIX - NetBSD\n");
+			break;
+		case ELFOSABI_LINUX:
+			printf("UNIX - Linux\n");
+			break;
+		case ELFOSABI_SOLARIS:
+			printf("UNIX - Solaris\n");
+			break;
+		case ELFOSABI_IRIX:
+			printf("UNIX - IRIX\n");
+			break;
+		case ELFOSABI_FREEBSD:
+			printf("UNIX - FreeBSD\n");
+			break;
+		case ELFOSABI_TRU64:
+			printf("UNIX - TRU64\n");
+			break;
+		case ELFOSABI_ARM:
+			printf("ARM\n");
+			break;
+		case ELFOSABI_STANDALONE:
+			printf("Standalone App\n");
+			break;
+		default:
+			printf("Unknown\n");
+			break;
 	}
 	printf("  ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
 	printf("  Type:                              ");
 	switch (header->e_type)
 	{
-		case ET_NONE:    printf("No file type\n"); break;
-		case ET_REL:     printf("Relocatable file\n"); break;
-		case ET_EXEC:    printf("Executable file\n"); break;
-		case ET_DYN:     printf("Shared object file\n"); break;
-		case ET_CORE:    printf("Core file\n"); break;
-		default:         printf("Unknown\n"); break;
+		case ET_NONE:
+			printf("No file type\n");
+			break;
+		case ET_REL:
+			printf("Relocatable file\n");
+			break;
+		case ET_EXEC:
+			printf("Executable file\n");
+			break;
+		case ET_DYN:
+			printf("Shared object file\n");
+			break;
+		case ET_CORE:
+			printf("Core file\n");
+			break;
+		default:
+			printf("Unknown\n");
+			break;
 	}
+	printf("  Machine:                           ");
+	switch (header->e_machine)
+	{
+		case EM_NONE:
+			printf("No machine\n");
+			break;
+		case EM_386:
+			printf("Intel 80386\n");
+			break;
+		case EM_X86_64:
+			printf("AMD x86-64 architecture\n");
+			break;
+		case EM_ARM:
+			printf("ARM\n");
+			break;
+		case EM_AARCH64:
+			printf("AArch64 (64-bit ARM)\n");
+			break;
+		case EM_MIPS:
+			printf("MIPS R3000\n");
+			break;
+		default:
+			printf("Unknown\n");
+			break;
+	}
+	printf("  Version:                           0x%x\n", header->e_version);
 	printf("  Entry point address:               0x%lx\n", header->e_entry);
+	printf("  Start of program headers:          %lu (bytes into file)\n", header->e_phoff);
+	printf("  Start of section headers:          %lu (bytes into file)\n", header->e_shoff);
+	printf("  Flags:                             0x%x\n", header->e_flags);
+	printf("  Size of this header:               %d (bytes)\n", header->e_ehsize);
+	printf("  Size of program headers:           %d (bytes)\n", header->e_phentsize);
+	printf("  Number of program headers:         %d\n", header->e_phnum);
+	printf("  Size of section headers:           %d (bytes)\n", header->e_shentsize);
+	printf("  Number of section headers:         %d\n", header->e_shnum);
+	printf("  Section header string table index: %d\n", header->e_shstrndx);
 }
+
 /**
  * main - Displays the information contained in the ELF header at the start of an ELF file.
  * @argc: Argument count
@@ -85,32 +166,27 @@ int main(int argc, char *argv[])
 	int fd;
 	Elf64_Ehdr header;
 
-	if (argc != 2)
+	if (argc < 2)
 	{
-		ERR("Usage: %s elf_filename", argv[0]);
-		return (98);
+		printf("Usage: %s <ELF binary>\n", argv[0]);
+		exit(EXIT_FAILURE);
 	}
 
 	fd = open(argv[1], O_RDONLY);
-
 	if (fd < 0)
 	{
 		ERR_SYSCALL(open);
-		return (1);
+		exit(EXIT_FAILURE);
 	}
+
 	if (read(fd, &header, sizeof(header)) != sizeof(header))
 	{
 		ERR_SYSCALL(read);
-		return (1);
+		exit(EXIT_FAILURE);
 	}
 
 	print_elf_header(&header);
 
-	if (close(fd) < 0)
-	{
-		ERR_SYSCALL(close);
-		return (1);
-	}
-
-	return (0);
+	close(fd);
+	return (EXIT_SUCCESS);
 }
