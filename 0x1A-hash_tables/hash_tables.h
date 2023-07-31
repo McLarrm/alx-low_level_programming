067de0cac7e80dc4f1426bef4438c0d54bcba52b
McLarrm <larrm_ola@live.com>
2023-07-31 12:08:00 -0700

    HEADER

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -26,4 +26,43 @@ char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
 
+/**
+ * struct shash_node_s - Node of a sorted hash table
+ * @key: The key
+ * @value: Value corresponding to a key
+ * @next: Pointer to the next node of the List
+ * @sprev: Pointer to the previous element of the linked list
+ * @snext: Pointer to the next element of the linked list
+ */
+typedef struct shash_node_s
+{
+	char *key;
+	char *value;
+	struct shash_node_s *next;
+	struct shash_node_s *sprev;
+	struct shash_node_s *snext;
+} shash_node_t;
+
+/**
+ * struct shash_table_s - Sorted hash table data structure
+ * @size: Size of the array
+ * @array: Array of size @size
+ * @shead: Pointer to the first element linked list
+ * @stail: Pointer to the last element linked list
+ */
+typedef struct shash_table_s
+{
+	unsigned long int size;
+	shash_node_t **array;
+	shash_node_t *shead;
+	shash_node_t *stail;
+} shash_table_t;
+
+shash_table_t *shash_table_create(unsigned long int size);
+int shash_table_set(shash_table_t *ht, const char *key, const char *value);
+char *shash_table_get(const shash_table_t *ht, const char *key);
+void shash_table_print(const shash_table_t *ht);
+void shash_table_print_rev(const shash_table_t *ht);
+void shash_table_delete(shash_table_t *ht);
+
 #endif
