e7cd85f399c43cfc385074ff87d02d009f5df182
McLarrm <larrm_ola@live.com>
2023-10-27 02:09:24 -0700

    Function that searches for a value in a sorted array of integers using the Exponential search algorithm

---
+++ 0x1E-search_algorithms/103-exponential.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include "search_algos.h"
+
+/**
+ * exponential_search - Searches for a value in a sorted array using
+ * the Exponential search algorithm.
+ * @array: A pointer to the first element of the array to search.
+ * @size: The number of elements in the array.
+ * @value: The value to search for.
+ * Return: The index of the value in the array or -1 if not found.
+ */
+int exponential_search(int *array, size_t size, int value)
+{
+	size_t bound = 1;
+	size_t low, high;
+
+	if (!array || size == 0)
+		return (-1);
+
+	while (bound < size && array[bound] < value)
+	{
+		printf("Value checked array[%lu] = [%d]\n", bound, array[bound]);
+		bound *= 2;
+	}
+
+	low = bound / 2;
+	high = (bound < size - 1) ? bound : size - 1;
+
+	printf("Value found between indexes [%lu] and [%lu]\n", low, high);
+	while (low <= high)
+	{
+		size_t mid = (low + high) / 2;
+
+		printf("Searching in array: ");
+		for (size_t i = low; i <= high; i++)
+		{
+			printf("%d", array[i]);
+			if (i < high)
+				printf(", ");
+			else
+				printf("\n");
+		}
+
+		if (array[mid] == value)
+			return (mid);
+
+		if (array[mid] < value)
+			low = mid + 1;
+		else
+			high = mid - 1;
+	}
+
+	return (-1);
+}
